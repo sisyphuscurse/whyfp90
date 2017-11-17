93ce8637d5239e652aab5d7b689199693e6e9044
Barry Zhong <zhongdj@gmail.com>
2017-11-17 22:39:50 +0800

    explicit cons compose f

--- src/main/scala/com/yiguan/compose/Composes.scala
+++ src/main/scala/com/yiguan/compose/Composes.scala
@@ -7,27 +7,25 @@ object Composes extends App {
   case object Nil extends List[Nothing]
 
   case class Cons[T](head: T, tail: List[T]) extends List[T]
+  private def cons[T]: (T) => List[T] => List[T] = (head: T) => (tail: List[T]) => Cons(head, tail)
 
   def foldr[A, E](f: E => A => A)(y: A)(xs: List[E]): A = xs match {
     case Nil => y
     case Cons(head: E, tail: List[E]) => f(head)(foldr(f)(y)(tail))
   }
 
-  def cons[T]: (T) => List[T] => List[T] = (head: T) => (tail: List[T]) => Cons(head, tail)
-
-  def double(n: Int): Int = 2 * n
-
   def doubleAll(xs: List[Int]): List[Int] = foldr(doubleAndCons)(Nil)(xs)
 
-  def doubleAndCons = fAndCons(double)
+  private def doubleAndCons = fAndCons(double)
 
-  def fAndCons[A, B](f: A => B) = cons[B].compose(f)
+  private def double(n: Int): Int = 2 * n
 
-  def map[A, B](f: A => B)(xs: List[A]): List[B] = foldr(fAndCons(f))(Nil)(xs)
+  private def fAndCons[A, B](f: A => B) = cons[B].compose(f)
 
   private val aList: List[Int] = Cons[Int](1, Cons[Int](2, Nil))
-
   println(doubleAll(aList))
 
+  def map[A, B](f: A => B)(xs: List[A]): List[B] = foldr(cons compose f)(Nil)(xs)
   println(map(double)(aList))
+
 }
